4cd98af2738ea635be7455b2634ff05f8397e8fb
Merve Unlu Menevse <m.merve.unlu@gmail.com>
2022-12-02 16:34:56 -0800

    edited script to get answer spans from textgrids

--- alignment.py
+++ alignment.py
@@ -1,37 +1,69 @@
 """
+This script finds the answer spans in text grid files and returns a dataframe as csv format.
+Args:
+    input: str, the input folder that contains textgrid files
+    data: str, the path of json file, should be SQuAD-like format
+    output: str, the path of the output file
 
+Usage:
+> python alignment.py --input textGrid/ \
+                      --data train-v1.1.json \
+                      --output train_answer_span.csv
 """
+import argparse
+import json
+import logging
+import os
+
+import pandas as pd
 import textgrid
+from tqdm import tqdm
 from utils import text_preprocess
+
+FORMAT = "%(asctime)s - %(name)s - %(levelname)s -[%(filename)s:%(lineno)s - %(funcName)20s() ]- %(message)s"
+logging.basicConfig(filename='log/data.log',
+                    level=logging.INFO,
+                    format=FORMAT,
+                    datefmt='%m/%d/%Y %I:%M:%S %p')
+
+logger = logging.getLogger(__name__)
 
 
 def find_timespan(tg_file, answer):
+    """
+    :param tg_file, str
+    :param answer, str
+    Returns
+       float, start time of the answer
+       float, end time of the answer
+    """
+    # opens textgrid file
     try:
         tg = textgrid.TextGrid.fromFile(tg_file)
     except:
         return [0], [0]
-    space_idx = [i for i in range(len(tg[0])) if tg[0][i].mark == '']
+    # gets the normalized words from the file
     words = [text_preprocess(tg[0][i].mark) for i in range(len(tg[0])) if tg[0][i].mark != '']
-
+    # gets the normalized words from the file including empty characters
     words_SIL = [text_preprocess(tg[0][i].mark) for i in range(len(tg[0]))]
 
     pos_map = [i for i, word in enumerate(words_SIL) if word != '']
-    answer = str(answer)
-
+    # sentence contains the context
     sentence = ' '.join(words)
 
-    # print(sentence)
+    # finds the starting point of the answer in the sentence
     match_idxs = [i for i in range(len(sentence)) if sentence.startswith(answer, i)]
-    # print(match_idxs)
+
+    # if no match, then return 0,0
     if len(match_idxs) == 0:
         return [0], [0]
     else:
+        # if match is found, then find start and end time spans
         start_times, end_times = [], []
         for match_idx in match_idxs:
             match_span = sentence[match_idx:match_idx + len(answer)]
 
             span = match_span.split()
-
             res = [words[idx:idx + len(span)] == span for idx in range(len(words))]
             try:
                 index = res.index(True)
@@ -43,5 +75,72 @@ def find_timespan(tg_file, answer):
         return start_times, end_times
 
 
-s, e = find_timespan('/home/daniel094144/Daniel/force_align/force_correct_dev/context-15_31.TextGrid',
-                     'accustomed union and the principle of non discrimination')
+def create_timespan_file(input_folder, answers_file, output_file):
+    """
+    This function creates a dataframe that contains time spans of the answers.
+    :param input_folder, str, the path of the input textgrid files
+    :param answers_file, str, original dataset json file
+    :param output_file, str, the path of the output csv file
+    Returns
+       None
+    """
+    logger.info("Opening the original data from %s" % answers_file)
+    # get the original dataset
+    with open(answers_file) as fp:
+        original_data = json.load(fp)['data']
+    # get a dict where keys are question ids and values are answer text
+    question2ans = []
+    for i, art in enumerate(original_data):
+        for j, p in enumerate(art['paragraphs']):
+            for qa in p['qas']:
+                question2ans.append({"id": qa['id'], "text": qa['answers'][0]['text'],
+                                     "context": "context-" + str(i) + "_" + str(j) + "_"})
+    logger.info("Total number of question is %d " % len(question2ans))
+    # first create a dict from original json
+    data_dict = []
+    input_files = sorted(os.listdir(input_folder))
+    for i, qa in tqdm(enumerate(question2ans)):
+        textfiles = [f for f in input_files if f.startswith(qa['context'])]
+        # search each file
+        for f in textfiles:
+            s, e = find_timespan(os.path.join(input_folder, f), text_preprocess(qa['text']))
+            # if answer is found in the file, then add it to dict
+            if s != [0] and e != [0]:
+                data_dict.append({"hash": qa['id'],
+                                  "text": qa['text'],
+                                  "utterance": f.replace(".TextGrid", ""),
+                                  "start": s[0],
+                                  "end": e[0]})
+        if i % 100 == 0:
+            logger.info("Processed %d numbers of example" % i)
+
+    # save the resulting file as csv
+    df = pd.DataFrame.from_dict(data_dict)
+    df.to_csv(output_file, sep=",", header=True, index=False)
+    logger.info("Resulting data is saved to %s " % output_file)
+
+
+def parse_args():
+    """ Argument parser"""
+    parser = argparse.ArgumentParser(description='Generate meta-file for given tsv file')
+    parser.add_argument('--input',
+                        help='input folder that contains TextGrid files',
+                        required=True)
+    parser.add_argument('--data',
+                        help='input json files that contains squad-format data',
+                        required=True)
+    parser.add_argument('--output',
+                        help='Output file name to save resulting csv',
+                        required=True)
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_args()
+    logger.info("Starting the script using input file %s" % args.input)
+    create_timespan_file(args.input, args.data, args.output)
+
+
+if __name__ == "__main__":
+    main()
